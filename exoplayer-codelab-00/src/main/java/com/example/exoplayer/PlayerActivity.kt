da2b0494244b13f759a183224e03d4f5c9b5e6f2
Rickson Lima <rickson.lima@sidia.com>
2023-10-07 10:23:28 -0400

    refactor: add hls converter

--- exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
+++ exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
@@ -1,112 +1,114 @@
-/*
- * Copyright (C) 2017 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
-* limitations under the License.
- */
 package com.example.exoplayer
-
-import android.annotation.SuppressLint
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
-import androidx.media3.common.util.Util
+import androidx.media3.common.Player
+import androidx.media3.common.Player.STATE_ENDED
+import androidx.media3.common.Player.STATE_READY
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DataSource
+import androidx.media3.datasource.DefaultHttpDataSource
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.hls.HlsMediaSource
+import androidx.media3.exoplayer.source.MediaSource
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
 import com.example.exoplayer.databinding.ActivityPlayerBinding
 
-/**
- * A fullscreen activity to play audio or video streams.
- */
-@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
-class PlayerActivity : AppCompatActivity() {
+@UnstableApi class PlayerActivity : AppCompatActivity() {
+//    private val mediaUrl = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4"
+//    private val mediaUrl = "http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8"
+    private val mediaUrl = "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.mp4/.m3u8"
+//    private val mediaUrl = "https://storage.googleapis.com/exoplayer-test-media-0/Jazz_In_Paris.mp3"
 
-    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
+    private val binding by lazy (LazyThreadSafetyMode.NONE) {
         ActivityPlayerBinding.inflate(layoutInflater)
     }
 
-    private var player: ExoPlayer? = null
-    private var playWhenReady = true
-    private var mediaItemIndex = 0
-    private var playbackPosition = 0L
-
+    private var player: ExoPlayer?= null
+    // Create a data source factory.
+    private val dataSourceFactory: DataSource.Factory = DefaultHttpDataSource.Factory()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(viewBinding.root)
+        setContentView(binding.root)
+        initPlayer()
     }
 
-    @SuppressLint("InlinedApi")
-    private fun hideSystemUi() {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        WindowInsetsControllerCompat(window, viewBinding.videoView).let { controller ->
-            controller.hide(WindowInsetsCompat.Type.systemBars())
-            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        }
-    }
-
-    public override fun onStart() {
-        super.onStart()
-        if (Util.SDK_INT > 23) {
-            initializePlayer()
-        }
-    }
-
-    public override fun onResume() {
-        super.onResume()
-        hideSystemUi()
-        if ((Util.SDK_INT <= 23 || player == null)) {
-            initializePlayer()
-        }
-    }
-
-    public override fun onPause() {
+    override fun onPause() {
         super.onPause()
-        if (Util.SDK_INT <= 23) {
-            releasePlayer()
-        }
+        pause()
     }
 
-
-    public override fun onStop() {
-        super.onStop()
-        if (Util.SDK_INT > 23) {
-            releasePlayer()
-        }
+    override fun onResume() {
+        super.onResume()
+        play()
     }
 
-    private fun releasePlayer() {
-        player?.let { exoPlayer ->
-            playbackPosition = exoPlayer.currentPosition
-            mediaItemIndex = exoPlayer.currentMediaItemIndex
-            playWhenReady = exoPlayer.playWhenReady
-            exoPlayer.release()
+    override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
+    }
+
+    private fun initPlayer(){
+        player = ExoPlayer.Builder(this)
+                .build()
+                .apply {
+
+                    val source = if (mediaUrl.contains("m3u8"))
+                        getHlsMediaSource()
+                    else
+                        getProgressiveMediaSource()
+
+                    setMediaSource(source)
+                    prepare()
+                    addListener(playerListener)
+                }
+    }
+
+    private fun getHlsMediaSource(): MediaSource {
+        // Create a HLS media source pointing to a playlist uri.
+        return HlsMediaSource.Factory(dataSourceFactory).
+        createMediaSource(MediaItem.fromUri(mediaUrl))
+    }
+
+    private fun getProgressiveMediaSource(): MediaSource{
+        // Create a Regular media source pointing to a playlist uri.
+        return ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(MediaItem.fromUri(Uri.parse(mediaUrl)))
+    }
+
+    private fun releasePlayer(){
+        player?.apply {
+            playWhenReady = false
+            release()
         }
         player = null
     }
 
+    private fun pause(){
+        player?.playWhenReady = false
+    }
 
-    private fun initializePlayer() {
-        player = ExoPlayer.Builder(this)
-                .build()
-                .also { exoPlayer ->
-                    viewBinding.videoView.player = exoPlayer
-                    val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
-//                    exoPlayer.setMediaItem(mediaItem)
-                    exoPlayer.setMediaItems(listOf(mediaItem), mediaItemIndex, playbackPosition)
-                    exoPlayer.playWhenReady = playWhenReady
-                    exoPlayer.prepare()
+    private fun play(){
+        player?.playWhenReady = true
+    }
+
+    private fun restartPlayer(){
+        player?.seekTo(0)
+        player?.playWhenReady = true
+    }
+
+    private val playerListener = object: Player.Listener {
+        override fun onPlaybackStateChanged(playbackState: Int) {
+            super.onPlaybackStateChanged(playbackState)
+            when(playbackState){
+                STATE_ENDED -> restartPlayer()
+                STATE_READY -> {
+                    binding.playerView.player = player
+                    play()
                 }
+            }
+        }
     }
 }
